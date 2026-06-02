b240a7ff2fa335894a6c6ab69dddeb6d56d4c03d
artyom <a.a.borodin@proton.me>
2026-06-02 15:48:13 +0300

    feat: disable some buttons on dashboard view

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -11,6 +11,7 @@
           :label="card.label"
           :icon="card.icon"
           :severity="card.severity"
+          :disabled="card.disabled"
           class="dashboard-btn"
           @click="goTo(card.route)"
         />
@@ -44,18 +45,21 @@ const dashboardCards = [
     label: APP_CONSTANTS.UI.LABELS.DOCUMENTS,
     icon: "pi pi-file",
     severity: "help",
+    disabled:true
   },
   {
     route: APP_CONSTANTS.ROUTES.DICTIONARIES,
     label: APP_CONSTANTS.UI.LABELS.DICTIONARIES,
     icon: "pi pi-database",
     severity: "warning",
+    disabled:true
   },
   {
     route: APP_CONSTANTS.ROUTES.STUDY_PLANS,
     label: APP_CONSTANTS.UI.LABELS.STUDY_PLANS,
     icon: "pi pi-sitemap",
     severity: "success",
+    disabled:true
   },
 ];
 
